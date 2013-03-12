3f91659c9c3ced5af48a5a2f489365979f270e1f
Amit Beka <amit.beka@mail.huji.ac.il>
2013-03-12 14:23:38 +0200

    scripts: beautify dixonization with comments and structure
    Signed-off-by: Amit Beka <amit.beka@mail.huji.ac.il>

--- scripts/dixonization.py
+++ scripts/dixonization.py
@@ -1,17 +1,25 @@
+"""Identifies and categorizes words according to Dixon's verb list."""
+
 import sys
 import xml.etree.ElementTree as ETree
+
 from ucca import lex, convert, scenes
 
 
-def main(path):
-    """Passage file ==> list of sceneness"""
-    with open(path) as f:
-        root = ETree.ElementTree().parse(f)
-    passage = convert.from_site(root)
-
+def main():
+    """Runs DixonIdentifier and gathers statistics."""
     eng = lex.DixonIdentifier(
         '/home/beka/thesis/resources/dixon-verbs.xml',
         '/home/beka/thesis/resources/collins/collins.pickle')
+    for path in sys.argv[1:]:
+        run_file(path, eng)
+
+
+def run_file(path, eng):
+    """Site XML file ==> prints list of sceneness results"""
+    with open(path) as f:
+        root = ETree.ElementTree().parse(f)
+    passage = convert.from_site(root)
 
     sc = scenes.extract_possible_scenes(passage)
     heads = [scenes.extract_head(x) for x in sc]
@@ -24,6 +32,6 @@ def main(path):
         print("Head: " + h.to_text())
         print("Categories: " + str(eng.get_categories(s, h)))
 
+
 if __name__ == '__main__':
-    for path in sys.argv[1:]:
-        main(path)
+    main()
