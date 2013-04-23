00ff303f04fc4d139c3a57dc4e949347c4baeb46
Amit Beka <amit.beka@mail.huji.ac.il>
2013-04-23 14:50:44 +0300

    features: add main and ngrams extraction API
    Signed-off-by: Amit Beka <amit.beka@mail.huji.ac.il>

--- features.py
+++ features.py
@@ -7,13 +7,45 @@ UCCA passages is also covered.
 
 """
 
+import argparse
+import sys
+
+from itertools import islice
+
+
+DEFAULT_CHUNK_SIZE = 2 ** 20
+
+
+def chunks(f, size=DEFAULT_CHUNK_SIZE):
+    """Yields chunks of lines from a file until exhausted.
+
+    Args:
+        f: file to yield lines from
+        size: hint to readlines() of how many bytes each yield should contain.
+
+    Yields:
+        list of strings (lines).
+
+    """
+    while True:
+        x = f.readlines(size)
+        if x:
+            yield x
+        else:
+            raise StopIteration
+
+
+def tokenize(sentences):
+    """list of string sentences ==> list of tuples of stripped tokens"""
+    return [tuple(x.strip().split()) for x in sentences if x.strip()]
+
 
 def extract_ngrams(size, sentences, given=None):
     """Extracts all ngrams of the given size from the sentences.
 
     Args:
         size: the N parameter of the ngrams to extract
-        sentences: sequence (can be generator) of lists of strings, each
+        sentences: sequence (can be generator) of tuples of strings, each
             is a token.
         given: previous ngram dictionary to start with. None otherwise.
             This dictionary isn't changed by the method.
@@ -23,13 +55,49 @@ def extract_ngrams(size, sentences, given=None):
 
     """
 
-    if given is None:
+    if given is not None:
         counts = given.copy()
     else:
         counts = {}
     for s in sentences:
         if len(s) < size:
             continue
-        for i in (len(s) - size + 1):
+        for i in range((len(s) - size + 1)):
             counts[s[i:i + size]] = counts.get(s[i:i + size], 0) + 1
     return counts
+
+
+def print_progress(current, updated=[0]):
+    """Prints progress to stderr by adding current to updated[0] each time."""
+    updated[0] = updated[0] + current
+    print("Processed: " + str(updated[0]), file=sys.stderr)
+
+
+def parse_cmd():
+    """Parses and validates cmd line arguments, then return them."""
+    parser = argparse.ArgumentParser()
+    parser.add_argument('command', choices=('ngrams',))
+    parser.add_argument('action', choices=('extract',))
+    parser.add_argument('--ngram_size', type=int, default=1)
+    parser.add_argument('--format', choices=('string', 'pickle'),
+                        default='string')
+
+    args = parser.parse_args()
+    return args
+
+
+def main():
+    args = parse_cmd()
+    if args.command == 'ngrams' and args.action == 'extract':
+        counts = None
+        for data in chunks(sys.stdin):
+            sentences = tokenize(data)
+            counts = extract_ngrams(args.ngram_size, sentences, counts)
+            print_progress(len(sentences))
+    sorted_counts = sorted(counts.items(), key=lambda x: x[1], reverse=True)
+    print('\n'.join('\t'.join([' '.join(x[0]), str(x[1])])
+                    for x in sorted_counts))
+
+
+if __name__ == '__main__':
+    main()
