88ca1b2e221ec065a4cc729f2f3120ba3b8c18c0
Amit Beka <amit.beka@mail.huji.ac.il>
2013-05-20 16:12:49 +0300

    classify: add create_feature_matrix
    Signed-off-by: Amit Beka <amit.beka@mail.huji.ac.il>

--- classify.py
+++ classify.py
@@ -13,3 +13,18 @@ def create_targets_array(targets_fd):
         targets.append(target)
         labels.append(label)
     return np.array(labels, dtype=np.int32), tuple(targets)
+
+
+def create_feature_matrix(scores_fd, targets, features):
+    """scores is target\tfeature_name\tscore, targets and features are tuples
+    of strings (without 'before'/'after' like feature_name) ==> feature matrix
+    which should have been sparse, but it's not."""
+    findex = lambda x: (features.index(x.split('_')[0])
+                        if x.endswith('_before')
+                        else features.index(x.split('_')[0]) + 1)
+
+    mat = np.zeros((len(targets), len(features) * 2))
+    for line in scores_fd:
+        t, f, s = line.strip().split('\t')  # target, feature name, score
+        mat[targets.index(t), findex(f)] = float(s)
+    return mat
