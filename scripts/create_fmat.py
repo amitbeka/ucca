c706b7c74365b15b819fbc82b763b4aa6adce390
Amit Beka <amit.beka@mail.huji.ac.il>
2013-06-09 14:43:50 +0300

    scripts: add create_fmat.py
    Transforms from features.py text-based output of morphological features
    to a numpy matrix in pickle file.
    
    Signed-off-by: Amit Beka <amit.beka@mail.huji.ac.il>

---
+++ scripts/create_fmat.py
@@ -0,0 +1,20 @@
+"""Converts a feature matrix as output by features.py morph extract
+(one line per feature, space-separated values, each line with len(targets))
+to a numpy feature matrix which can be used to classification."""
+import pickle
+import sys
+import numpy as np
+
+
+def main():
+    with open(sys.argv[1]) as f:
+        feature_lines = [x.strip().split() for x in f]
+    feature_data = [[float(x) for x in line] for line in feature_lines]
+    targets_data = zip(*feature_data)
+    fmat = np.array(targets_data)
+    with open(sys.argv[2], 'wb') as f:
+        pickle.dump(fmat, f)
+
+
+if __name__ == '__main__':
+    main()
