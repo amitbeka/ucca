7660ca708eaf50e2609f8184b3ff9fb82cc015a2
Amit Beka <amit.beka@mail.huji.ac.il>
2013-07-28 00:49:31 +0300

    classify: add self training function
    Signed-off-by: Amit Beka <amit.beka@mail.huji.ac.il>

--- classify.py
+++ classify.py
@@ -1,4 +1,5 @@
 import mlpy
+import random
 import numpy as np
 from ucca import lex
 
@@ -142,3 +143,76 @@ def return_new_labels(probs, confidence_0, confidence_1):
         elif prob >= confidence_1:
             labels[i] = 1
     return labels
+
+
+def split_arrays(arr, values, *additional_arrays):
+    indexers = [arr == value for value in values]
+    split_arr = np.array([arr[indexer] for indexer in indexers])
+    if not additional_arrays:
+        return split_arr
+    split_all = [split_arr]
+    for array in additional_arrays:
+        split_all.append(np.array([array[indexer] for indexer in indexers]))
+    return split_all
+
+
+PARAM_PRE_LABELS, PARAM_CONF0, PARAM_CONF1 = range(3)
+PRE_LABELS_THRESH = 3
+
+
+def self_train_classifier(fmat_all, pre_scores, targets_all, params_list,
+                          method='c_svc_prob'):
+
+    split_point = np.array([len(pre_scores)])
+    pre_fmat, unlabeled_fmat = np.split(fmat_all, split_point)
+    pre_targets, unlabeled_targets = np.split(targets_all, split_point)
+    unlabeled_labels = np.zeros(len(unlabeled_targets), dtype=np.int32)
+    unlabeled_labels.fill(UNDECISIVE_LABEL)
+    pre_labels, pre_targets, pre_fmat = split_arrays(pre_scores, range(6),
+                                                     pre_targets, pre_fmat)
+    for pre_value in range(6):
+        if pre_value < PRE_LABELS_THRESH:
+            pre_labels[pre_value].fill(0)
+        else:
+            pre_labels[pre_value].fill(1)
+
+    for params in params_list:
+        # taking the relevant pre-labeled data
+        pre_labels_to_take = params[PARAM_PRE_LABELS]
+        labels = pre_labels[pre_labels_to_take][0]
+        targets = pre_targets[pre_labels_to_take][0]
+        fmat = pre_fmat[pre_labels_to_take][0]
+        for score in pre_labels_to_take[1:]:
+            labels = np.append(labels, pre_labels[score])
+            targets = np.append(targets, pre_targets[score])
+            fmat = np.append(fmat, pre_fmat[score], 0)
+
+        # taking confident unlabeled data
+        unlabel_to_take = (unlabeled_labels != UNDECISIVE_LABEL)
+        labels = np.append(labels, unlabeled_labels[unlabel_to_take])
+        targets = np.append(targets, unlabeled_targets[unlabel_to_take])
+        fmat = np.append(fmat, unlabeled_fmat[unlabel_to_take], 0)
+
+        # splitting to 0/1 and creating similar-size training sets
+        labels, targets, fmat = split_arrays(labels, [0, 1], targets, fmat)
+        if len(labels[0]) > 2 * len(labels[1]):
+            to_take = list(range(len(labels[0])))
+            random.shuffle(to_take)
+            to_take = to_take[:2 * len(labels[1])]
+            labels[0] = labels[0][to_take]
+            targets[0] = targets[0][to_take]
+            fmat[0] = fmat[0][to_take]
+        labels = np.append(labels[0], labels[1])
+        targets = np.append(targets[0], targets[1])
+        fmat = np.append(fmat[0], fmat[1], 0)
+
+        # training a classifier and predicting
+        clas = train_classifier(fmat, labels, method)
+        if hasattr(clas, 'pred_probability'):
+            _, probs = zip(*clas.pred_probability(unlabeled_fmat))
+        else:
+            _, probs = zip(*clas.pred_proba(unlabeled_fmat))
+        unlabeled_labels = return_new_labels(probs, params[PARAM_CONF0],
+                                             params[PARAM_CONF1])
+
+    return clas, unlabeled_targets, unlabeled_labels
